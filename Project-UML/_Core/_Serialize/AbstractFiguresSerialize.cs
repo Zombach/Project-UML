b2a6082d656e43ee334ac000eb917c0a6d8d4982
Zombach <kozlov.s.v.1992@gmail.com>
2021-04-12 03:40:12 +0300

    AbstractFigures

---
+++ Project-UML/_Core/_Serialize/AbstractFiguresSerialize.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_UML._Core._Arrows;
+//using Project_UML._Core._Boxs;
+
+namespace Project_UML._Core._Serialize
+{
+    public class AbstractFiguresSerialize
+    {
+        public AbstractFiguresSerialize(AbstractArrow arrow)
+        {
+            StructSerialize tmpStruct = new StructSerialize();
+            //tmpStruct.Color = arrow.Color;
+            //tmpStruct.Width = arrow.Width;
+            //tmpStruct.Type = arrow.Type;
+            //tmpStruct.Points.
+        }
+        //public AbstractFiguresSerialize(AbstractBox box)
+        //{
+        //    tmpStruct tmpStruct = new StructSerialize();
+        //    tmpStruct.Color = box.Color;
+        //    tmpStruct.Width = box.Width;
+        //}
+    }
+}
