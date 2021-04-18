0dbfbe58394f43e7f9be5cad51bcad21d3f96c97
Zombach <kozlov.s.v.1992@gmail.com>
2021-04-18 14:41:04 +0300

    SerializeUpd

---
+++ Project-UML/Core/Serialize/InterfacesSerialize/IFigures.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_UML.Core.Serialize.InterfacesSerialize
+{
+    public interface IFigures : IBox, IArrow
+    {
+
+        List<DataCommon> Data { get; set; }
+        Color Color { get; set; }
+        int Width { get; set; }        
+        float Size { get; set; }
+    }
+}
