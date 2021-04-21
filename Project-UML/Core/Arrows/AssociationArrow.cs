9704eb9906597ba3dbd0f81f01690c9c88417ddd
Timur <Kotafrakt@gmail.com>
2021-04-22 01:07:42 +0300

    set pink color of about us

--- Project-UML/Core/Arrows/AssociationArrow.cs
+++ Project-UML/Core/Arrows/AssociationArrow.cs
@@ -35,26 +35,32 @@ namespace Project_UML.Core.Arrows
         public override void SetEndCap()
         {
             int tmp;
+            GraphicsPath _graphicsPath = new GraphicsPath();
             if (_pen.Width == 1)
             {
-                tmp = 2;
+                tmp = 1;
+                _graphicsPath.AddLine(new Point(0, 0), new Point(8 / tmp, -20 / tmp));
+                _graphicsPath.AddLine(new Point(8 / tmp, -20 / tmp), new Point(0, 0));
+                _graphicsPath.AddLine(new Point(0, 0), new Point(-8 / tmp, -20 / tmp));
+                _graphicsPath.AddLine(new Point(-8 / tmp, -20 / tmp), new Point(0, 0));
             }
             else
             {
+                tmp = 2;
+                _graphicsPath.AddLine(new Point(0, 0), new Point(8 / tmp, -20 / tmp));
+                _graphicsPath.AddLine(new Point(8 / tmp, -20 / tmp), new Point(0, 0));
+                _graphicsPath.AddLine(new Point(0, 0), new Point(-8 / tmp, -20 / tmp));
+                _graphicsPath.AddLine(new Point(-8 / tmp, -20 / tmp), new Point(0, 0));
 
-                tmp = 1;
             }
-            GraphicsPath _graphicsPath = new GraphicsPath();
-            _graphicsPath.AddLine(new Point(0, 0), new Point(10 / tmp, -24 / tmp));
-            _graphicsPath.AddLine(new Point(10 / tmp, -24 / tmp), new Point(0, 0));
-            _graphicsPath.AddLine(new Point(0, 0), new Point(-10 / tmp, -24 / tmp));
-            _graphicsPath.AddLine(new Point(-10 / tmp, -24 / tmp), new Point(0, 0));
             _pen.CustomEndCap = new CustomLineCap(null, _graphicsPath, LineCap.Custom);
+
         }
 
         public override void ChangeWidth(int width)
         {
             _pen.Width = width;
+            SetEndCap();
         }
     }
 }
--- Project-UML/Core/FormsUML/AboutUs.Designer.cs
+++ Project-UML/Core/FormsUML/AboutUs.Designer.cs
@@ -146,7 +146,7 @@ namespace Project_UML.Core.FormsUML
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Moccasin;
+            this.BackColor = System.Drawing.Color.Pink;
             this.ClientSize = new System.Drawing.Size(993, 512);
             this.Controls.Add(this.pictureBox9);
             this.Controls.Add(this.pictureBox8);
